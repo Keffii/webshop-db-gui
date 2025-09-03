749ac2017fc5cc3a32ee303d3d1f0972cfe1308c
Keffii <59940915+Keffii@users.noreply.github.com>
2025-09-03 19:38:11 +0200

    Update Main.java

--- src/webshop_db/Main.java
+++ src/webshop_db/Main.java
@@ -1,11 +1,8 @@
 package webshop_db;
 
-import java.io.IOException;
-import java.sql.SQLException;
-
 public class Main {
     public static void main(String[] args) throws Exception {
-        Query1FindBlackPants db = new Query1FindBlackPants();
-        db.DB(); // anropa databaskoden
+        SqlQueriesWithSwing db = new SqlQueriesWithSwing();
+        db.DB(); // Set up database data for JDBC queries
     }
 }
